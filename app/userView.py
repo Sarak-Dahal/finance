082a3d03a32c72ecf0fd0b6ce3b324a034d16f9a
Sarak Dahal <ssagamersdignity@gmail.com>
2022-09-09 15:17:50 +0545

    First commit

--- app/userView.py
+++ app/userView.py
@@ -4,3 +4,7 @@ from app import app
 @app.route('/')
 def login():
     return render_template('login.html')
+@app.route('/',methods = ['POST', 'GET'])
+def webLogin():
+    return render_template('login.html')
+
