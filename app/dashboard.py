535de3ae7f37e02185304b30bd2365c81e874295
Sarak Dahal <ssagamersdignity@gmail.com>
2022-09-13 22:05:43 +0545

    First commit

--- app/dashboard.py
+++ app/dashboard.py
@@ -6,13 +6,41 @@ from app import app
 def home():
     return render_template('userTemplate/home.html')
 
+@app.route('/dataAnalytics')
+def dataAnalytics():
+    return render_template('userTemplate/dataAnalytics.html')
+
+
+@app.route('/portfolioSessions')
+def portfolioSessions():
+    return render_template('userTemplate/portfolioSessions.html')
+
+
+@app.route('/createSession')
+def createSession():
+    return render_template('userTemplate/createSession.html')
+
+@app.route('/monteCarlo')
+def monteCarlo():
+    return render_template('userTemplate/monteCarlo.html')
+
+
+@app.route('/regimeChanging')
+def regimeChanging():
+    return render_template('userTemplate/regimeChanging.html')
+
+
+@app.route('/portfolioReview')
+def portfolioReview():
+    return render_template('userTemplate/portfolioReview.html')
+
 
 @app.route('/user')
 def dashboardUser():
     return render_template('userTemplate/user.html')
 
 
-
 @app.route('/logout')
 def logout():
     return render_template('login.html')
+
--- app/userView.py
+++ app/userView.py
@@ -9,7 +9,7 @@ from email.message import EmailMessage
 
 def connection():
     s = 'guideportfolios.database.windows.net'  # Your server name
-    d = 'InvServ_DEV'
+    d = 'InvServ_DEV' # Database Name
     u = 'guideadmin'  # Your login
     p = 'Tbg963369@'  # Your login password
     cstr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + s + ';DATABASE=' + d + ';UID=' + u + ';PWD=' + p
@@ -31,25 +31,19 @@ def webLogin():
         try:
             conn = connection()
             cursor = conn.cursor()
-            cursor.execute('''Select * FROM dbo.userDetails WHERE (Name = '?') and (Pass ='?') ''', username, password)
+            cursor.execute("Select * from dbo.userDetails WHERE name = '" + username + "' AND password = '" + password + "'")
+            data = cursor.fetchone()
             conn.close()
-            print('Login Successful')
-            return redirect('dashboard')
+            if data:
+                print('Login Successful')
+                return render_template('userTemplate/user.html')
+            else:
+                print('Database Error')
+                return redirect('/')
         except:
             print('Login Failed')
             return redirect('/')
-    return render_template('/')
-
-
-def databaseTest():
-    users = []
-    conn = connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM dbo.userDetails")
-    for row in cursor.fetchall():
-        users.append({"id": row[0], "name": row[1], "number": row[2], "email": row[3], "password": row[4]})
-    conn.close()
-    return render_template('dashboard.html', users=users)
+    return render_template('login.html')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -68,11 +62,11 @@ def signUp():
                 conn.commit()
                 conn.close()
                 print('User created Successfully')
-                return redirect('login')
+                return render_template('login.html')
             except:
                 print('User not created')
                 msg = "Please provide different Value."
-                return render_template('/')
+                return render_template('login.html')
 
         else:
             return render_template('login.html')
@@ -92,7 +86,7 @@ def forgotPassword():
         try:
             conn = connection()
             cursor = conn.cursor()
-            cursor.execute('''Select * FROM dbo.userDetails WHERE (Name = '?') and (Pass ='?') ''', number, email)
+            cursor.execute("Select * from dbo.userDetails WHERE number = '" + number + "' AND email = '" + email + "'")
             success = cursor.fetchone()
 
             if success:
@@ -106,29 +100,50 @@ def forgotPassword():
                 try:
                     conn = connection()
                     cursor = conn.cursor()
-                    cursor.execute('''Insert into dbo.userDetails (Name,Number,Email,Password) VALUES(?,?,?,?)''', temp)
+                    cursor.execute("Update dbo.userDetails set password = '" + temp + "' where email = '" + email + "'")
                     conn.commit()
-                    msg.set_content(
-                        "Your new Password is: " + temp + " \n Do not share this password with anyone else. If you need additional help you may mail to youremail@gmail.com")
-                    msg['Subject'] = 'Nepse Master Password Reset'
-                    msg['From'] = "nepsemaster@gmail.com"
-                    msg['To'] = email
-                    # send secret key to mail
-                    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-                    server.login("nepsemaster@gmail.com", "$h!$h!r9861")
-                    server.send_message(msg)
-                    server.send
-                    server.quit()
+                    forgot = cursor.fetchone()
+                    conn.close()
+                    if forgot:
+                        msg.set_content(
+                            "Your new Password is: " + temp + "\n Do not share this password with anyone else. If you "
+                                                              "need additional help you may mail to youremail@gmail.com")
+                        msg['Subject'] = 'Nepse Master Password Reset'
+                        msg['From'] = "nepsemaster@gmail.com"
+                        msg['To'] = email
+                        # send secret key to mail
+                        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                        server.login("nepsemaster@gmail.com", "$h!$h!r9861")
+                        server.send_message(msg)
+                        server.send
+                        server.quit()
+                        print('Database connection done')
+                        return render_template('login.html')
+                    else:
+                        print('Database connection could not be made.')
+                        return render_template('forgotPassword.html')
                 except:
                     print('Database connection could not be made.')
+                    return render_template('forgotPassword.html')
             else:
                 msg = "Invalid Email or Phone Number entered"
                 print('Invalid credentials')
-            conn.close()
-            return redirect('dashboard')
+                return render_template('login.html')
         except:
-            return redirect('login')
+            return render_template('login.html')
     return render_template('login.html')
+# def databaseTest():
+#     users = []
+#     conn = connection()
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT * FROM dbo.userDetails")
+#     for row in cursor.fetchall():
+#         users.append({"id": row[0], "name": row[1], "number": row[2], "email": row[3], "password": row[4]})
+#     conn.close()
+#     return render_template('dashboard.html', users=users)
+#
+
+
 
 # Select [Date],
 # Price,
